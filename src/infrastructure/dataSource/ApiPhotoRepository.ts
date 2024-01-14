a144a6d054820f74a0af5b1949e69e457a429cf2
OscarGimenez <o.gimenez.aldabas@gmail.com>
2024-01-14 17:57:17 +0100

    refactor: :recycle: remove cache from adapter

--- src/infrastructure/dataSource/ApiPhotoRepository.ts
+++ src/infrastructure/dataSource/ApiPhotoRepository.ts
@@ -2,14 +2,8 @@ import { type IPhoto } from '@/domain/models/IPhoto';
 import { type IPhotoRepository } from '@/domain/ports/IPhotoRepository';
 
 export function createApiPhotoRepository(): IPhotoRepository {
-  const cache = new Map<number, IPhoto>();
-
   async function list(): Promise<IPhoto[]> {
     const source = 'https://jsonplaceholder.typicode.com/photos';
-
-    if (cache.size > 0) {
-      return Array.from(cache.values());
-    }
 
     const res = await fetch(source);
     if (!res.ok) {
@@ -17,7 +11,6 @@ export function createApiPhotoRepository(): IPhotoRepository {
     }
 
     const photos = await res.json();
-    photos.forEach((photo: IPhoto) => cache.set(photo.id, photo));
 
     return photos;
   }
