36b6e72c1ce0e2f7de253e03ed667e5db76d16ed
OscarGimenez <o.gimenez.aldabas@gmail.com>
2024-01-14 10:08:57 +0100

    chore: :construction: prepare for production deployment

---
+++ webpack.config.prod.js
@@ -0,0 +1,67 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const { dependencies } = require('./package.json');
+
+module.exports = {
+  name: 'SushiMicroFrontendPhotos',
+  entry: './src/index.ts',
+  mode: 'production',
+  devtool: 'eval-source-map',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', { loader: 'css-loader', options: { importLoaders: 1 } }, 'postcss-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+      filename: 'index.html'
+    }),
+    new ModuleFederationPlugin({
+      name: 'SushiMicroFrontendPhotos',
+      filename: 'remoteEntry.js',
+      remotes: {},
+      exposes: {
+        './SushiMicroFrontendPhotos': './src/App.tsx'
+      },
+      shared: {
+        react: {
+          singleton: true,
+          requiredVersion: dependencies.react
+        },
+        'react-dom': {
+          singleton: true,
+          requiredVersion: dependencies['react-dom']
+        },
+        'react-error-boundary': {
+          singleton: true,
+          requiredVersion: dependencies['react-error-boundary']
+        }
+      }
+    })
+  ]
+};
