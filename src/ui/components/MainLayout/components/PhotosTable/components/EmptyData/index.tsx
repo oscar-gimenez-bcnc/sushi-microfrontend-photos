a893dacfe12265d6be86152b701ccde95a55213e
OscarGimenez <o.gimenez.aldabas@gmail.com>
2024-01-14 18:35:32 +0100

    fix: :bug: remove warning td inside div

--- src/ui/components/MainLayout/components/PhotosTable/components/EmptyData/index.tsx
+++ src/ui/components/MainLayout/components/PhotosTable/components/EmptyData/index.tsx
@@ -2,14 +2,10 @@ import IconInfo from '@/ui/components/shared/icons/IconInfo';
 
 const EmptyData: React.FC = () => {
   return (
-    <tr>
-      <td colSpan={8} className="text-center">
-        <div role="alert" className="alert alert-info">
-          <IconInfo />
-          <span>There is no data here.</span>
-        </div>
-      </td>
-    </tr>
+    <div role="alert" className="alert alert-info w-full">
+      <IconInfo />
+      <span>There is no data here.</span>
+    </div>
   );
 };
 
